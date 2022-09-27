9f3c86dedab2ae3edc61ddbd22b95368b73a18fc
Robert Szafarczyk <robertszafa@gmail.com>
2022-09-27 17:29:00 +0100

    Make test inputs parameterizable via command args.

--- inputs/histogram/histogram.cpp
+++ inputs/histogram/histogram.cpp
@@ -1,14 +1,21 @@
 #include <CL/sycl.hpp>
 #include <algorithm>
+#include <iostream>
 #include <numeric>
-#include <sycl/ext/intel/fpga_extensions.hpp>
+#include <stdlib.h>
 #include <vector>
+#include <random>
 
+#include <sycl/ext/intel/fpga_extensions.hpp>
+
+#include "store_queue.hpp"
 #include "memory_utils.hpp"
 
 using namespace sycl;
 
+// Forward declare kernel name.
 class MainKernel;
+
 double histogram_kernel(queue &q, const std::vector<int> &h_idx, std::vector<float> &h_hist) {
   const int array_size = h_idx.size();
 
@@ -17,11 +24,13 @@ double histogram_kernel(queue &q, const std::vector<int> &h_idx, std::vector<flo
 
   auto event = q.submit([&](handler &hnd) {
     hnd.single_task<MainKernel>([=]() [[intel::kernel_args_restrict]] {
+      /////////////////////////////////// KERNEL CODE /////////////////////////////////////////////
       for (int i = 0; i < array_size; ++i) {
         auto idx_scalar = idx[i];
-        float x = hist[idx_scalar];
-        hist[idx_scalar] = x + 10.0f;
+        auto x = hist[idx_scalar];
+        hist[idx_scalar] = x + 10.0;
       }
+      /////////////////////////////////// KERNEL CODE /////////////////////////////////////////////
     });
   });
 
@@ -41,8 +50,31 @@ double histogram_kernel(queue &q, const std::vector<int> &h_idx, std::vector<flo
 void histogram_cpu(const int *idx, float *hist, const int N) {
   for (int i = 0; i < N; ++i) {
     auto idx_scalar = idx[i];
-    float x = hist[idx_scalar];
-    hist[idx_scalar] = x + 10.0f;
+    auto x = hist[idx_scalar];
+    hist[idx_scalar] = x + 10.0;
+  }
+}
+
+enum data_distribution { ALL_WAIT, NO_WAIT, PERCENTAGE_WAIT };
+void init_data(std::vector<int> &feature, std::vector<int> &hist,
+               const data_distribution distr, const int percentage) {
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution(0, 100);
+  auto dice = std::bind (distribution, generator);
+
+  int counter=0;
+  for (int i = 0; i < feature.size(); i++) {
+    if (distr == data_distribution::ALL_WAIT) {
+      feature[i] = (feature.size() >= 4) ? std::max(i-1, 0) : i % feature.size();
+    }
+    else if (distr == data_distribution::NO_WAIT) {
+      feature[i] = i;
+    }
+    else {
+      feature[i] = (dice() <= percentage) ? feature[std::max(i-1, 0)] : i;
+    }
+
+    hist[i] = 0;
   }
 }
 
@@ -61,6 +93,31 @@ static auto exception_handler = [](sycl::exception_list e_list) {
 };
 
 int main(int argc, char *argv[]) {
+  // Get A_SIZE and forward/no-forward from args.
+  int ARRAY_SIZE = 64;
+  auto DATA_DISTR = data_distribution::ALL_WAIT;
+  int PERCENTAGE = 5;
+  try {
+    if (argc > 1) {
+      ARRAY_SIZE = int(atoi(argv[1]));
+    }
+    if (argc > 2) {
+      DATA_DISTR = data_distribution(atoi(argv[2]));
+    }
+    if (argc > 3) {
+      PERCENTAGE = int(atoi(argv[3]));
+      std::cout << "Percentage is " << PERCENTAGE << "\n";
+      if (PERCENTAGE < 0 || PERCENTAGE > 100)
+        throw std::invalid_argument("Invalid percentage.");
+    }
+  } catch (exception const &e) {
+    std::cout << "Incorrect argv.\nUsage:\n";
+    std::cout << "  ./executable [ARRAY_SIZE] [data_distribution (0/1/2)] [PERCENTAGE (only for "
+                 "data_distr 2)]\n";
+    std::cout << "    0 - all_wait, 1 - no_wait, 2 - PERCENTAGE wait\n";
+    std::terminate();
+  }
+
 #if FPGA_EMULATOR
   ext::intel::fpga_emulator_selector d_selector;
 #elif FPGA
@@ -75,8 +132,6 @@ int main(int argc, char *argv[]) {
 
     // Print out the device information used for the kernel code.
     std::cout << "Running on device: " << q.get_device().get_info<info::device::name>() << "\n";
-
-    int ARRAY_SIZE = 16;
 
     std::vector<int> feature(ARRAY_SIZE);
     std::vector<float> hist(ARRAY_SIZE);
@@ -101,9 +156,8 @@ int main(int argc, char *argv[]) {
       std::cout << "Passed\n";
     } else {
       std::cout << "Failse\n";
-      std::cout << " sum(hist) = " << std::accumulate(hist.begin(), hist.end(), 0.0) << "\n";
-      std::cout << " sum(hist_cpu) = " << std::accumulate(hist_cpu.begin(), hist_cpu.end(), 0.0)
-                << "\n";
+      std::cout << "sum(fpga) = " << std::accumulate(hist.begin(), hist.end(), 0.0) << "\n";
+      std::cout << "sum(cpu) = " << std::accumulate(hist_cpu.begin(), hist_cpu.end(), 0.0) << "\n";
     }
   } catch (exception const &e) {
     std::cout << "An exception was caught.\n";
@@ -112,3 +166,4 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+
--- inputs/histogram_if/histogram_if.cpp
+++ inputs/histogram_if/histogram_if.cpp
@@ -1,14 +1,21 @@
 #include <CL/sycl.hpp>
 #include <algorithm>
+#include <iostream>
 #include <numeric>
-#include <sycl/ext/intel/fpga_extensions.hpp>
+#include <stdlib.h>
 #include <vector>
+#include <random>
 
+#include <sycl/ext/intel/fpga_extensions.hpp>
+
+#include "store_queue.hpp"
 #include "memory_utils.hpp"
 
 using namespace sycl;
 
+// Forward declare kernel name.
 class MainKernel;
+
 double histogram_if_kernel(queue &q, const std::vector<int> &h_idx, const std::vector<int> &h_weight,
                            std::vector<float> &h_hist) {
   const int array_size = h_idx.size();
@@ -19,6 +26,7 @@ double histogram_if_kernel(queue &q, const std::vector<int> &h_idx, const std::v
 
   auto event = q.submit([&](handler &hnd) {
     hnd.single_task<MainKernel>([=]() [[intel::kernel_args_restrict]] {
+      /////////////////////////////////// KERNEL CODE /////////////////////////////////////////////
       for (int i = 0; i < array_size; ++i) {
         auto wt = weight[i];
         auto idx_scalar = idx[i];
@@ -27,6 +35,7 @@ double histogram_if_kernel(queue &q, const std::vector<int> &h_idx, const std::v
         if (wt > 0)
           hist[idx_scalar] = x + 10.0;
       }
+      /////////////////////////////////// KERNEL CODE /////////////////////////////////////////////
     });
   });
 
@@ -54,6 +63,30 @@ void histogram_if_cpu(const int *idx, const int *weight, float *hist, const int 
   }
 }
 
+enum data_distribution { ALL_WAIT, NO_WAIT, PERCENTAGE_WAIT };
+void init_data(std::vector<int> &feature, std::vector<int> &weight, std::vector<int> &hist,
+               const data_distribution distr, const int percentage) {
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution(0, 100);
+  auto dice = std::bind (distribution, generator);
+
+  int counter=0;
+  for (int i = 0; i < feature.size(); i++) {
+    if (distr == data_distribution::ALL_WAIT) {
+      feature[i] = (feature.size() >= 4) ? std::max(i-1, 0) : i % feature.size();
+    }
+    else if (distr == data_distribution::NO_WAIT) {
+      feature[i] = i;
+    }
+    else {
+      feature[i] = (dice() <= percentage) ? feature[std::max(i-1, 0)] : i;
+    }
+
+    weight[i] = (i % 2 == 0) ? 1 : 0;
+    hist[i] = 0;
+  }
+}
+
 // Create an exception handler for asynchronous SYCL exceptions
 static auto exception_handler = [](sycl::exception_list e_list) {
   for (std::exception_ptr const &e : e_list) {
@@ -69,6 +102,31 @@ static auto exception_handler = [](sycl::exception_list e_list) {
 };
 
 int main(int argc, char *argv[]) {
+  // Get A_SIZE and forward/no-forward from args.
+  int ARRAY_SIZE = 64;
+  auto DATA_DISTR = data_distribution::ALL_WAIT;
+  int PERCENTAGE = 5;
+  try {
+    if (argc > 1) {
+      ARRAY_SIZE = int(atoi(argv[1]));
+    }
+    if (argc > 2) {
+      DATA_DISTR = data_distribution(atoi(argv[2]));
+    }
+    if (argc > 3) {
+      PERCENTAGE = int(atoi(argv[3]));
+      std::cout << "Percentage is " << PERCENTAGE << "\n";
+      if (PERCENTAGE < 0 || PERCENTAGE > 100)
+        throw std::invalid_argument("Invalid percentage.");
+    }
+  } catch (exception const &e) {
+    std::cout << "Incorrect argv.\nUsage:\n";
+    std::cout << "  ./executable [ARRAY_SIZE] [data_distribution (0/1/2)] [PERCENTAGE (only for "
+                 "data_distr 2)]\n";
+    std::cout << "    0 - all_wait, 1 - no_wait, 2 - PERCENTAGE wait\n";
+    std::terminate();
+  }
+
 #if FPGA_EMULATOR
   ext::intel::fpga_emulator_selector d_selector;
 #elif FPGA
@@ -83,8 +141,6 @@ int main(int argc, char *argv[]) {
 
     // Print out the device information used for the kernel code.
     std::cout << "Running on device: " << q.get_device().get_info<info::device::name>() << "\n";
-
-    int ARRAY_SIZE = 16;
 
     std::vector<int> feature(ARRAY_SIZE);
     std::vector<int> weight(ARRAY_SIZE);
@@ -112,9 +168,8 @@ int main(int argc, char *argv[]) {
       std::cout << "Passed\n";
     } else {
       std::cout << "Failse\n";
-      std::cout << " sum(hist) = " << std::accumulate(hist.begin(), hist.end(), 0.0) << "\n";
-      std::cout << " sum(hist_cpu) = " << std::accumulate(hist_cpu.begin(), hist_cpu.end(), 0.0)
-                << "\n";
+      std::cout << "sum(fpga) = " << std::accumulate(hist.begin(), hist.end(), 0.0) << "\n";
+      std::cout << "sum(cpu) = " << std::accumulate(hist_cpu.begin(), hist_cpu.end(), 0.0) << "\n";
     }
   } catch (exception const &e) {
     std::cout << "An exception was caught.\n";
@@ -123,3 +178,4 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+

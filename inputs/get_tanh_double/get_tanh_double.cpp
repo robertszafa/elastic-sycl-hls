4d6a1dbb77ea9aeedd5ae5c3fdc53727b30687f3
Robert Szafarczyk <robertszafa@gmail.com>
2022-11-11 15:45:22 +0000

    Add two more benchmarks.

---
+++ inputs/get_tanh_double/get_tanh_double.cpp
@@ -0,0 +1,186 @@
+#include <CL/sycl.hpp>
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <stdlib.h>
+#include <vector>
+#include <random>
+
+#include <sycl/ext/intel/fpga_extensions.hpp>
+
+#include "store_queue.hpp"
+#include "memory_utils.hpp"
+
+using namespace sycl;
+
+// Forward declare kernel name.
+class MainKernel;
+
+
+double get_tanh_double_kernel(queue &q, std::vector<double> &h_A, const std::vector<int> h_addr_in,
+                       const std::vector<int> h_addr_out) {
+  const uint array_size = h_A.size();
+
+  double* A = toDevice(h_A, q);
+  int* addr_in = toDevice(h_addr_in, q);
+  int* addr_out = toDevice(h_addr_out, q);
+
+  auto event = q.submit([&](handler &hnd) {
+    hnd.single_task<MainKernel>([=]() [[intel::kernel_args_restrict]] {
+      double result, is_neg, beta;
+      for (int i = 0; i < array_size; i++) {
+        beta = A[addr_in[i]];
+
+        result = ((beta*beta+19.52381)*beta*beta+3.704762)*beta;
+
+        A[addr_out[i]] = result;
+      }
+    });
+  });
+
+  event.wait();
+  q.copy(A, h_A.data(), h_A.size()).wait();
+
+  sycl::free(A, q);
+  sycl::free(addr_in, q);
+  sycl::free(addr_out, q);
+
+  auto start = event.get_profiling_info<info::event_profiling::command_start>();
+  auto end = event.get_profiling_info<info::event_profiling::command_end>();
+  double time_in_ms = static_cast<double>(end - start) / 1000000;
+
+  return time_in_ms;
+}
+
+
+enum data_distribution { ALL_WAIT, NO_WAIT, PERCENTAGE_WAIT };
+void init_data(std::vector<double> &A, std::vector<int> &addr_in, std::vector<int> &addr_out,
+               const data_distribution distr, const uint percentage) {
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution(0, 100);
+  auto dice = std::bind(distribution, generator);
+
+  addr_in[0] = 0;
+  addr_in[1] = 1;
+  for (int i = 0; i < A.size(); i++) {
+    A[i] = i % 2 == 0? ((double)rand()/RAND_MAX*2.0-1.0) : 1.2;
+
+    if (distr == data_distribution::ALL_WAIT) {
+      addr_in[i] = std::max(i - 1, 0);
+      addr_out[i] = std::min(max(i + 1, 0), int(A.size()-1));
+    } else if (distr == data_distribution::NO_WAIT) {
+      addr_in[i] = i;
+      addr_out[i] = i;
+    } else {
+      addr_in[i] = (dice() <= percentage) ? addr_in[std::max(i - 1, 0)] : i;
+      addr_out[i] = addr_in[i];
+    }
+  }
+}
+
+
+void get_tanh_double_cpu(std::vector<double> &A, const std::vector<int> addr_in,
+                  const std::vector<int> addr_out) {
+  int i;
+  double result, is_neg, beta;
+
+  for (i = 0; i < A.size(); i++) {
+    beta = A[addr_in[i]];
+
+    result = ((beta*beta+19.52381)*beta*beta+3.704762)*beta;
+
+    A[addr_out[i]] = result;
+  }
+}
+
+// Create an exception handler for asynchronous SYCL exceptions
+static auto exception_handler = [](sycl::exception_list e_list) {
+  for (std::exception_ptr const &e : e_list) {
+    try {
+      std::rethrow_exception(e);
+    } catch (std::exception const &e) {
+#if _DEBUG
+      std::cout << "Failure" << std::endl;
+#endif
+      std::terminate();
+    }
+  }
+};
+
+int main(int argc, char *argv[]) {
+  // Get A_SIZE and forward/no-forward from args.
+  // defaulats
+  uint ARRAY_SIZE = 64;
+  auto DATA_DISTR = data_distribution::ALL_WAIT;
+  uint PERCENTAGE = 5;
+  try {
+    if (argc > 1) {
+      ARRAY_SIZE = uint(atoi(argv[1]));
+    }
+    if (argc > 2) {
+      DATA_DISTR = data_distribution(atoi(argv[2]));
+    }
+    if (argc > 3) {
+      PERCENTAGE = uint(atoi(argv[3]));
+      std::cout << "Percentage is " << PERCENTAGE << "\n";
+      if (PERCENTAGE < 0 || PERCENTAGE > 100)
+        throw std::invalid_argument("Invalid percentage.");
+    }
+  } catch (exception const &e) {
+    std::cout << "Incorrect argv.\nUsage:\n";
+    std::cout << "  ./hist [ARRAY_SIZE] [data_distribution (0/1/2)] [PERCENTAGE (only for "
+                 "data_distr 2)]\n";
+    std::cout << "    0 - all_wait, 1 - no_wait, 2 - PERCENTAGE wait\n";
+    std::terminate();
+  }
+
+#if FPGA_EMULATOR
+  ext::intel::fpga_emulator_selector d_selector;
+#elif FPGA
+  ext::intel::fpga_selector d_selector;
+#else
+  default_selector d_selector;
+#endif
+  try {
+    // Enable profiling.
+    property_list properties{property::queue::enable_profiling()};
+    queue q(d_selector, exception_handler, properties);
+
+    // Print out the device information used for the kernel code.
+    std::cout << "Running on device: " << q.get_device().get_info<info::device::name>() << "\n";
+
+    std::vector<double> A(ARRAY_SIZE);
+    std::vector<int> addr_in(ARRAY_SIZE);
+    std::vector<int> addr_out(ARRAY_SIZE);
+
+    init_data(A, addr_in, addr_out, DATA_DISTR, PERCENTAGE);
+
+    std::vector<double> A_cpu(ARRAY_SIZE);
+    std::copy(A.begin(), A.end(), A_cpu.begin());
+
+    auto start = std::chrono::steady_clock::now();
+    double kernel_time = 0;
+
+    kernel_time = get_tanh_double_kernel(q, A, addr_in, addr_out);
+
+    // Wait for all work to finish.
+    q.wait();
+
+    std::cout << "\nKernel time (ms): " << kernel_time << "\n";
+
+    get_tanh_double_cpu(A_cpu, addr_in, addr_out);
+    if (std::equal(A.begin(), A.end(), A_cpu.begin())) {
+      std::cout << "Passed\n";
+    } else {
+      std::cout << "Failed";
+      std::cout << " sum(A_fpga) = " << std::accumulate(A.begin(), A.end(), 0) << "\n";
+      std::cout << " sum(A_cpu) = " << std::accumulate(A_cpu.begin(), A_cpu.end(), 0) << "\n";
+    }
+  } catch (exception const &e) {
+    std::cout << "An exception was caught.\n";
+    std::terminate();
+  }
+
+  return 0;
+}
+
---
+++ inputs/sssp/sssp.cpp
@@ -0,0 +1,231 @@
+#include <CL/sycl.hpp>
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <stdlib.h>
+#include <vector>
+#include <random>
+
+#include <sycl/ext/intel/fpga_extensions.hpp>
+
+#include "store_queue.hpp"
+#include "memory_utils.hpp"
+
+using namespace sycl;
+
+// Forward declare kernel name.
+class MainKernel;
+
+double sssp_kernel(queue & q, const std::vector<int> &h_G, std::vector<int> &h_cost,
+                   std::vector<int> &h_distance, std::vector<int> &h_visited,
+                   std::vector<int> &h_pred, int numNodes, int startNode) {
+
+  int *G = fpga_tools::toDevice(h_G, q);
+  int *cost = fpga_tools::toDevice(h_cost, q);
+  int *distance = fpga_tools::toDevice(h_distance, q);
+  int *visited = fpga_tools::toDevice(h_visited, q);
+  int *pred = fpga_tools::toDevice(h_pred, q);
+
+  auto event = q.submit([&](handler &hnd) {
+    hnd.single_task<MainKernel>([=]() [[intel::kernel_args_restrict]] {
+      int count = 1;
+      while (count < numNodes - 1) {
+        int mindistance = INT_MAX;
+        int nextnode = 0;
+        // nextnode gives the node at minimum distance
+        // #pragma unroll
+        for (int i = 0; i < numNodes; i++) {
+          if (distance[i] < mindistance && !visited[i]) {
+            mindistance = distance[i];
+            nextnode = i;
+          }
+        }
+
+        // check if a better path exists through nextnode
+        visited[nextnode] = 1;
+        // #pragma unroll
+        for (int i = 0; i < numNodes; i++) {
+          if (!visited[i] && mindistance + cost[nextnode * numNodes + i] < distance[i]) {
+            distance[i] = mindistance + cost[nextnode * numNodes + i];
+            pred[i] = nextnode;
+          }
+        }
+        count++;
+      }
+
+    });
+  });
+
+  event.wait();
+  q.copy(distance, h_distance.data(), h_distance.size()).wait();
+
+  sycl::free(G, q);
+  sycl::free(cost, q);
+  sycl::free(distance, q);
+  sycl::free(visited, q);
+  sycl::free(pred, q);
+
+
+  auto start = event.get_profiling_info<info::event_profiling::command_start>();
+  auto end = event.get_profiling_info<info::event_profiling::command_end>();
+  double time_in_ms = static_cast<double>(end - start) / 1000000;
+
+  return time_in_ms;
+}
+
+void sssp_cpu(const std::vector<int> &G, std::vector<int> &cost,
+                   std::vector<int> &distance, std::vector<int> &visited,
+                   std::vector<int> &pred, int numNodes, int startNode) {
+  int count = 1;
+  while (count < numNodes - 1) {
+    int mindistance = INT_MAX;
+    int nextnode = 0;
+    // nextnode gives the node at minimum distance
+    for (int i = 0; i < numNodes; i++) {
+      if (distance[i] < mindistance && !visited[i]) {
+        mindistance = distance[i];
+        nextnode = i;
+      }
+    }
+
+    // check if a better path exists through nextnode
+    visited[nextnode] = 1;
+    for (int i = 0; i < numNodes; i++) {
+      if (!visited[i] && mindistance + cost[nextnode * numNodes + i] < distance[i]) {
+        distance[i] = mindistance + cost[nextnode * numNodes + i];
+        pred[i] = nextnode;
+      }
+    }
+    count++;
+  }
+}
+
+enum data_distribution { ALL_WAIT, NO_WAIT, PERCENTAGE_WAIT };
+void init_data(std::vector<int> &G, std::vector<int> &cost, std::vector<int> &distance,
+               std::vector<int> &visited, std::vector<int> &pred, int numNodes, int startNode) {
+  
+  // Create adjencency matrix
+  for (int i = 0; i < numNodes; i++) {
+    for (int j = 0; j < numNodes; j++) {
+      G[i * numNodes + j] = 0;
+      if (i != j && (i + j) % 2 == 0) {
+        G[i * numNodes + j] = 1;
+      }
+    }
+  }
+
+  // int cost[MAX][MAX],distance[MAX],pred[MAX];
+  // int visited[MAX],count,mindistance,nextnode,i,j;
+  // pred[] stores the predecessor of each node
+  // count gives the number of nodes seen so far
+  // create the cost matrix
+  for (int i = 0; i < numNodes; i++) {
+    for (int j = 0; j < numNodes; j++) {
+      if (G[i * numNodes + j] == 0)
+        cost[i*numNodes + j] = INT_MAX;
+      else
+        cost[i*numNodes + j] = G[i*numNodes + j];
+    }
+  }
+  // initialize pred[],distance[] and visited[]
+  for (int i = 0; i < numNodes; i++) {
+    distance[i] = cost[startNode *numNodes + i];
+    pred[i] = startNode;
+    visited[i] = 0;
+  }
+  distance[startNode] = 0;
+  visited[startNode] = 1;
+}
+
+// Create an exception handler for asynchronous SYCL exceptions
+static auto exception_handler = [](sycl::exception_list e_list) {
+  for (std::exception_ptr const &e : e_list) {
+    try {
+      std::rethrow_exception(e);
+    } catch (std::exception const &e) {
+#if _DEBUG
+      std::cout << "Failure" << std::endl;
+#endif
+      std::terminate();
+    }
+  }
+};
+
+int main(int argc, char *argv[]) {
+  // Get A_SIZE and forward/no-forward from args.
+  int NUM_NODES = 200;
+
+  auto DATA_DISTR = data_distribution::ALL_WAIT;
+  int PERCENTAGE = 5;
+  try {
+    if (argc > 1) {
+      NUM_NODES = int(atoi(argv[1]));
+    }
+    if (argc > 2) {
+      DATA_DISTR = data_distribution(atoi(argv[2]));
+    }
+    if (argc > 3) {
+      PERCENTAGE = int(atoi(argv[3]));
+      std::cout << "Percentage is " << PERCENTAGE << "\n";
+      if (PERCENTAGE < 0 || PERCENTAGE > 100)
+        throw std::invalid_argument("Invalid percentage.");
+    }
+  } catch (exception const &e) {
+    std::cout << "Incorrect argv.\nUsage:\n";
+    std::cout << "  ./executable [num_items] [data_distribution (0/1/2)] [PERCENTAGE (only for "
+                 "data_distr 2)]\n";
+    std::cout << "    0 - all_wait, 1 - no_wait, 2 - PERCENTAGE wait\n";
+    std::terminate();
+  }
+
+#if FPGA_EMULATOR
+  ext::intel::fpga_emulator_selector d_selector;
+#elif FPGA
+  ext::intel::fpga_selector d_selector;
+#else
+  default_selector d_selector;
+#endif
+  try {
+    // Enable profiling.
+    property_list properties{property::queue::enable_profiling()};
+    queue q(d_selector, exception_handler, properties);
+
+    // Print out the device information used for the kernel code.
+    std::cout << "Running on device: " << q.get_device().get_info<info::device::name>() << "\n";
+
+    std::vector<int> G(NUM_NODES * NUM_NODES);
+    std::vector<int> cost(NUM_NODES * NUM_NODES);
+    std::vector<int> distance(NUM_NODES);
+    std::vector<int> visited(NUM_NODES);
+    std::vector<int> pred(NUM_NODES);
+    int startNode = 0;
+
+    init_data(G, cost, distance, visited, pred, NUM_NODES, startNode);
+    std::vector<int> distance_cpu(NUM_NODES);
+    std::copy_n(distance.data(), NUM_NODES, distance_cpu.data());
+
+    auto start = std::chrono::steady_clock::now();
+    double kernel_time = 0;
+
+    kernel_time = sssp_kernel(q, G, cost, distance, visited, pred, NUM_NODES, startNode);
+
+    // Wait for all work to finish.
+    q.wait();
+
+    std::cout << "\nKernel time (ms): " << kernel_time << "\n";
+
+    sssp_cpu(G, cost, distance_cpu, visited, pred, NUM_NODES, startNode);
+    if (std::equal(distance.begin(), distance.end(), distance_cpu.begin())) {
+      std::cout << "Passed\n";
+    } else {
+      std::cout << "Failed\n";
+    }
+
+  } catch (exception const &e) {
+    std::cout << "An exception was caught.\n";
+    std::terminate();
+  }
+
+  return 0;
+}
+

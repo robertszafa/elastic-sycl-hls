deb585a96b48fcc9df390f314f28f5e31d540b3b
Robert Szafarczyk <robertszafa@gmail.com>
2024-03-04 16:10:51 +0000

    Change load port and load mux predicates.

--- lsq/TaggedStreamingMemoryMultiKernel.hpp
+++ lsq/TaggedStreamingMemoryMultiKernel.hpp
@@ -48,14 +48,14 @@ template <typename T> struct addr_tag_val_t {
   T val;
 };
 
-
+using uint2_t = ac_int<2, false>;
+enum LOAD_MUX_PRED { TERMINATE, REUSE, LOAD };
 
 /// Unique kernel name generators.
-template <int MemId> class StreamingLoadKernel;
-template <int MemId> class LoadPortKernel;
-template <int MemId> class StorePortKernel;
-template <int MemId> class LoadValMuxKernel;
-template <int MemId> class StreamingStoreKernel;
+template <int ID> class StreamingLoadKernel;
+template <int ID> class LoadPortKernel;
+template <int ID> class LoadValMuxKernel;
+template <int ID> class StreamingStoreKernel;
 
 template <int id, typename LoadAddrPipe, typename LoadValPipe, 
           typename StoreAddrPipe, typename StoreValPipe, typename StoreAckPipe, 
@@ -63,38 +63,42 @@ template <int id, typename LoadAddrPipe, typename LoadValPipe,
 event StreamingLoad(queue &q, T *data) {
   assert(sizeof(T) * 8 == BIT_WIDTH && "Incorrect kBitWidth.");
   constexpr uint NUM_BURST_VALS = (DRAM_BURST_BITS + BIT_WIDTH - 1) / BIT_WIDTH;
+  constexpr uint LOAD_PORT_Q_SIZE = NUM_BURST_VALS * 4;
+  constexpr uint STORE_Q_SIZE = NUM_BURST_VALS;
+  constexpr uint LOAD_Q_SIZE = 2;
 
-  using LoadPortAddr = pipe<class _LoadAddr, int, NUM_BURST_VALS * 4>;
-  using LoadPortPred = pipe<class _LoadPortPred, bool, NUM_BURST_VALS * 4>;
-  using LoadMuxIsReuse = pipe<class _LoadMuxIsReuse, bool, NUM_BURST_VALS * 4>;
-  using LoadMuxPred = pipe<class _LoadMuxPred, bool, NUM_BURST_VALS * 4>;
-  using LoadMuxMemoryVal = pipe<class _LoadMuxMemoryVal, T, NUM_BURST_VALS * 4>;
-  using LoadMuxReuseVal = pipe<class _LoadMuxReuseVal, T, NUM_BURST_VALS * 4>;
+  using LoadPortAddr = pipe<class _LoadAddr, int, LOAD_PORT_Q_SIZE>;
+  using LoadMuxMemoryVal = pipe<class _LoadMuxMemoryVal, T, LOAD_PORT_Q_SIZE>;
   q.single_task<LoadPortKernel<id>>([=]() KERNEL_PRAGMAS {
     [[intel::ivdep]]
     [[intel::initiation_interval(1)]]
     [[intel::speculated_iterations(0)]]
-    while (LoadPortPred::read()) {
+    while (true) {
       int Addr = LoadPortAddr::read();
+      if (Addr == MAX_INT) break;
       auto LoadPtr = ext::intel::device_ptr<T>(data + Addr);
       auto Val = BurstCoalescedLSU::load(LoadPtr);
       LoadMuxMemoryVal::write(Val);
     }
-    // Ensure all stores commited before finishing kernel.
-    // PRINTF("DONE load port %d\n", id);
   });
+  using LoadMuxPred = pipe<class _LoadMuxPred, uint2_t, LOAD_PORT_Q_SIZE>;
+  using LoadMuxReuseVal = pipe<class _LoadMuxReuseVal, T, LOAD_PORT_Q_SIZE>;
   q.single_task<LoadValMuxKernel<id>>([=]() KERNEL_PRAGMAS {
     int NumTotal = 0;
     int NumReused = 0;
-    while (LoadMuxPred::read()) {
-      NumTotal++;
+    while (true) {
+      uint2_t pred = LoadMuxPred::read();
+
       T val;
-      if (LoadMuxIsReuse::read()) {
+      if (pred == uint2_t{LOAD_MUX_PRED::REUSE}) {
         NumReused++;
         val = LoadMuxReuseVal::read();
-      } else {
+      } else if (pred == uint2_t{LOAD_MUX_PRED::LOAD}) {
         val = LoadMuxMemoryVal::read();
+      } else {
+        break;
       }
+      NumTotal++;
       LoadValPipe::write(val);
     }
     PRINTF("DONE load MUX, reused %d/%d\n", NumReused, NumTotal);
@@ -110,12 +114,11 @@ event StreamingLoad(queue &q, T *data) {
     T StoreVal = {};
     uint StoreValTag = 0;
 
-    DataBundle<uint, NUM_BURST_VALS> StoreAllocTag(uint{0});
-    DataBundle<bool, NUM_BURST_VALS> StoreAllocValid(bool{false});
-    StoreAllocTag[NUM_BURST_VALS - 1] = LastStoreAllocTag;
-    StoreAllocValid[NUM_BURST_VALS - 1] = true;
+    DataBundle<uint, STORE_Q_SIZE> StoreAllocTag(uint{0});
+    DataBundle<bool, STORE_Q_SIZE> StoreAllocValid(bool{false});
+    StoreAllocTag[STORE_Q_SIZE - 1] = LastStoreAllocTag;
+    StoreAllocValid[STORE_Q_SIZE - 1] = true;
 
-    constexpr uint LOAD_Q_SIZE = 2;
     DataBundle<uint, LOAD_Q_SIZE> LoadAddr(uint{0});
     DataBundle<uint, LOAD_Q_SIZE> LoadTag(uint{MAX_INT});
     DataBundle<bool, LOAD_Q_SIZE> LoadAddrValid(bool{false});
@@ -158,7 +161,7 @@ event StreamingLoad(queue &q, T *data) {
 
       // TODO: How to delay this if (storeAddr > loadAddr) ?
       /** Rule for reading store {addr, tag} pairs. */
-      if (!StoreAllocValid[NUM_BURST_VALS - 1] &&
+      if (!StoreAllocValid[STORE_Q_SIZE - 1] &&
           (LastStoreAllocAddr <= LoadAddr[0] ||
            LastStoreAllocTag >= LoadTag[0])) {
         bool succ = false;
@@ -166,8 +169,8 @@ event StreamingLoad(queue &q, T *data) {
         if (succ) {
           LastStoreAllocAddr = StoreReq.addr;
           LastStoreAllocTag = StoreReq.tag;
-          StoreAllocValid[NUM_BURST_VALS - 1] = true;
-          StoreAllocTag[NUM_BURST_VALS - 1] = LastStoreAllocTag;
+          StoreAllocValid[STORE_Q_SIZE - 1] = true;
+          StoreAllocTag[STORE_Q_SIZE - 1] = LastStoreAllocTag;
         }
       }
       /** End Rule */
@@ -195,19 +198,14 @@ event StreamingLoad(queue &q, T *data) {
       if (LoadAddrValid[0]) {
         if (LoadReuse[0]) {
           if (LoadWaitForTag[0] == StoreValTag) {
-            LoadMuxPred::write(1);
-            LoadMuxIsReuse::write(1);
+            LoadMuxPred::write(uint2_t{LOAD_MUX_PRED::REUSE});
             LoadMuxReuseVal::write(StoreVal);
-
             LoadAddrValid[0] = false;
           }
         } else if (LoadSafeNow[0] || (LoadSafeAfterTag[0] &&
                                       (LoadWaitForTag[0] <= StoreAckTag))) {
-          LoadMuxPred::write(1);
-          LoadMuxIsReuse::write(0);
-          LoadPortPred::write(1);
+          LoadMuxPred::write(uint2_t{LOAD_MUX_PRED::LOAD});
           LoadPortAddr::write(LoadAddr[0]);
-
           LoadAddrValid[0] = false;
         } 
       }
@@ -240,8 +238,8 @@ event StreamingLoad(queue &q, T *data) {
 
     } // end while
 
-    LoadMuxPred::write(0);
-    LoadPortPred::write(0);
+    LoadMuxPred::write(uint2_t{LOAD_MUX_PRED::TERMINATE});
+    LoadPortAddr::write(MAX_INT);
 
     PRINTF("DONE Streaming Load %d\n", id);
   });

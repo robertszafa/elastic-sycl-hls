c208d54d58e1360dbfbc59308fc6d6d68abee193
Robert Szafarczyk <robertszafa@gmail.com>
2024-06-18 13:48:52 +0100

    Add prints of: dep info, pipe defs, and decoupled kernels.

--- lib/DynamicLoopFusionAnalysisPrinter.cpp
+++ lib/DynamicLoopFusionAnalysisPrinter.cpp
@@ -1,9 +1,4 @@
-/// This file is the analysis driver. It uses DataHazardAnalysis and
-/// CDDDAnalysis to identify memory instructions, basic blocks, and whole loops
-/// which can benefit from dynamic scheduling.
-
 #include "CommonLLVM.h"
-#include "AnalysisReportSchema.h"
 #include "DynamicLoopFusionAnalysis.h"
 
 using namespace llvm;
@@ -12,138 +7,233 @@ using namespace llvm;
 namespace llvm {
 
 using MemoryRequest = DynamicLoopFusionAnalysis::MemoryRequest;
+using DecoupledLoopInfo = DynamicLoopFusionAnalysis::DecoupledLoopInfo;
+using MemoryDependencyInfo = DynamicLoopFusionAnalysis::MemoryDependencyInfo;
+using DepDir = DynamicLoopFusionAnalysis::DepDir;
 
-/// Given a basic block, return the name of the kernel that will contain it
-/// after our transformations.
-std::string getKernelName(BasicBlock *BB, const SmallVector<PEInfo> &peArray) {
-  // If the BB is decoupled into a block PE, then return its name.
-  for (auto &peInfo : peArray) {
-    if (peInfo.peType == BLOCK && peInfo.basicBlock == BB)
-      return peInfo.peKernelName;
-  }
-  // Otherwise, if the BB is part of a loop PE, then return its name.
-  for (auto &peInfo : peArray) {
-    if (peInfo.peType == LOOP && peInfo.loop->contains(BB))
-      return peInfo.peKernelName;
-  }
-  // Otherwise, the BB will not be decoupled. Return the original kernel name.
-  return demangle(std::string(BB->getParent()->getName()));
-}
+std::string getDepInfoStructDef(MemoryDependencyInfo &DepI) {
+  std::string res;
+  llvm::raw_string_ostream O(res);
 
-int getCommonLoopDepth(Loop *L1, Loop *L2) {
-  if (L1 == L2) 
-    return L1->getLoopDepth() - 1;
-  else if (L1->getLoopDepth() == 0 || L2->getLoopDepth() == 0)
-    return -1;
-
-  if (L1->getLoopDepth() > L2->getLoopDepth()) 
-    return getCommonLoopDepth(L1->getParentLoop(), L2);
-  else
-    return getCommonLoopDepth(L1, L2->getParentLoop());
-
-  return -1;
-}
-
-/// Return the direction of the A --depends-on--> B dependency. Return
-/// ENUM_DIR::BACK if A comes first in program order, else return FORWARD.
-DEP_DIR getDependencyDir(Instruction *A, Instruction *B, LoopInfo &LI) {
-  auto LA = LI.getLoopFor(A->getParent());
-  auto LB = LI.getLoopFor(B->getParent());
-
-  if (LA == LB) {
-    if (A->getParent() == B->getParent()) {
-      return getIndexIntoParent(A) < getIndexIntoParent(B) ? BACK : FORWARD;
+  auto print1D = [&O] (auto vec1d) {
+    O << "{";
+    for (auto elem : vec1d) 
+      O << elem << ", ";
+    O << "}";
+  };
+  auto print2D = [&print1D, &O] (auto vec2d) {
+    O << "{\n    ";
+    for (auto vec1d : vec2d) {
+      print1D(vec1d);
+      O << ", ";
     }
-    
-    for (auto BB : LA->blocks()) {
-      if (BB == A->getParent()) {
-        return BACK;
-      } else if (BB == B->getParent()) {
-        return FORWARD;
-      }
-    }
-  }
+    O << "\n  }";
+  };
 
-  for (auto L : LI.getLoopsInPreorder()) {
-    if (L == LA) {
-      return BACK;
-    } else if (L == LB) {
-      return FORWARD;
-    }
-  }
-
-  return FORWARD;
-}
-
-SmallVector<MemoryDependencyInfo, 4>
-getMemoryDependencyInfo(DynamicLoopFusionAnalysis &DLFA, LoopInfo &LI) {
-  MapVector<int, SmallVector<MemoryRequest *>> LoadsForMem =
-      DLFA.getCluesteredLoadRequests();
-  MapVector<int, SmallVector<MemoryRequest *>> StoresForMem =
-      DLFA.getCluesteredStoreRequests();
-
-  // init
-  MapVector<int, MemoryDependencyInfo> DepInfoForMem;
-  for (auto kv : StoresForMem) {
-    DepInfoForMem[kv.first] = MemoryDependencyInfo{
-        kv.first,
-        int(LoadsForMem[kv.first].size()),
-        int(StoresForMem[kv.first].size()),
-    };
-  }
-
-  for (auto &[Id, LdRequests] : LoadsForMem) {
-    for (auto &LdReq : LdRequests) {
-      auto LdLoop = LI.getLoopFor(LdReq->memOp->getParent());
-      DepInfoForMem[Id].maxLoopDepth = std::max(DepInfoForMem[Id].maxLoopDepth, 
-                                                LdReq->loopDepth + 1);
-
-      DepInfoForMem[Id].loadLoopDepth.push_back(LdReq->loopDepth);
-      DepInfoForMem[Id].loadIsMaxIterNeeded.push_back(LdReq->isMaxIterNeeded);
-
-      SmallVector<bool> StoreInSameLoop;
-      SmallVector<int> StoreCommonLoopDepth;
-      SmallVector<DEP_DIR> StoreDepDir;
-      for (auto &StReq : StoresForMem[Id]) {
-        auto StLoop = LI.getLoopFor(StReq->memOp->getParent());
-        StoreInSameLoop.push_back(LdLoop == StLoop);
-        StoreCommonLoopDepth.push_back(getCommonLoopDepth(LdLoop, StLoop));
-        StoreDepDir.push_back(getDependencyDir(LdReq->memOp, StReq->memOp, LI));
-      }
-      DepInfoForMem[Id].loadStoreInSameLoop.push_back(StoreInSameLoop);
-      DepInfoForMem[Id].loadStoreCommonLoopDepth.push_back(StoreCommonLoopDepth);
-      DepInfoForMem[Id].loadStoreDepDir.push_back(StoreDepDir);
-    }
-  }
-
-  for (auto &[Id, StRequests] : StoresForMem) {
-    for (auto &StReq : StRequests) {
-      auto StReqLoop = LI.getLoopFor(StReq->memOp->getParent());
-      DepInfoForMem[Id].maxLoopDepth = std::max(DepInfoForMem[Id].maxLoopDepth, 
-                                                StReq->loopDepth + 1);
-
-      DepInfoForMem[Id].storeLoopDepth.push_back(StReq->loopDepth);
-      DepInfoForMem[Id].storeIsMaxIterNeeded.push_back(StReq->isMaxIterNeeded);
-
-      SmallVector<bool> StoreInSameLoop;
-      SmallVector<int> StoreCommonLoopDepth;
-      SmallVector<DEP_DIR> StoreDepDir;
-      for (auto &OtherStReq : StoresForMem[Id]) {
-        auto OtherStLoop = LI.getLoopFor(OtherStReq->memOp->getParent());
-        StoreInSameLoop.push_back(StReqLoop == OtherStLoop);
-        StoreCommonLoopDepth.push_back(getCommonLoopDepth(StReqLoop, OtherStLoop));
-        StoreDepDir.push_back(getDependencyDir(StReq->memOp, OtherStReq->memOp, LI));
-      }
-      DepInfoForMem[Id].storeStoreInSameLoop.push_back(StoreInSameLoop);
-      DepInfoForMem[Id].storeStoreCommonLoopDepth.push_back(StoreCommonLoopDepth);
-      DepInfoForMem[Id].storeStoreDepDir.push_back(StoreDepDir);
-    }
-  }
-
-  SmallVector<MemoryDependencyInfo, 4> Res;
-  for (auto &kv : DepInfoForMem)
-    Res.push_back(kv.second);
+  O << "\ntemplate <> struct DepInfo<" << DepI.id << "> {\n";
+  O << "  static constexpr int NUM_LOADS = " << DepI.numLoads << ";\n";
+  O << "  static constexpr int NUM_STORES = " << DepI.numStores << ";\n";
+  O << "  static constexpr int MAX_LOOP_DEPTH = " << DepI.maxLoopDepth << ";\n\n";
   
+  // Loads
+  O << "  static constexpr int LOAD_LOOP_DEPTH[NUM_LOADS] = ";
+  print1D(DepI.loadLoopDepth);
+  O << ";\n";
+
+  O << "  static constexpr bool LOAD_IS_MAX_ITER_NEEDED[NUM_LOADS][MAX_LOOP_DEPTH] = ";
+  print2D(DepI.loadIsMaxIterNeeded);
+  O << ";\n";
+
+  O << "  static constexpr bool LOAD_STORE_IN_SAME_LOOP[NUM_LOADS][NUM_STORES] = ";
+  print2D(DepI.loadStoreInSameLoop);
+  O << ";\n";
+
+  O << "  static constexpr int LOAD_STORE_COMMON_LOOP_DEPTH[NUM_LOADS][NUM_STORES] = ";
+  print2D(DepI.loadStoreCommonLoopDepth);
+  O << ";\n";
+
+  O << "  static constexpr DEP_DIR LOAD_STORE_DEP_DIR[NUM_LOADS][NUM_STORES] = ";
+  print2D(DepI.loadStoreDepDir);
+  O << ";\n\n";
+
+  // Stores
+  O << "  static constexpr int STORE_LOOP_DEPTH[NUM_STORES] = ";
+  print1D(DepI.storeLoopDepth);
+  O << ";\n";
+
+  O << "  static constexpr bool STORE_IS_MAX_ITER_NEEDED[NUM_STORES][MAX_LOOP_DEPTH] = ";
+  print2D(DepI.storeIsMaxIterNeeded);
+  O << ";\n";
+
+  O << "  static constexpr bool STORE_STORE_IN_SAME_LOOP[NUM_STORES][NUM_STORES] = ";
+  print2D(DepI.storeStoreInSameLoop);
+  O << ";\n";
+
+  O << "  static constexpr int STORE_STORE_COMMON_LOOP_DEPTH[NUM_STORES][NUM_STORES] = ";
+  print2D(DepI.storeStoreCommonLoopDepth);
+  O << ";\n";
+
+  O << "  static constexpr int STORE_STORE_DEP_DIR[NUM_STORES][NUM_STORES] = ";
+  print2D(DepI.storeStoreDepDir);
+  O << ";\n\n";
+
+  O << "}; // end DepInfo \n";
+
+  return res;
+}
+
+std::string getPipeDefString(MemoryDependencyInfo &MemDep) {
+  std::string res;
+  llvm::raw_string_ostream O(res);
+
+  constexpr unsigned kReqPipeDepth = 16;
+
+  O << llvm::formatv(
+    "using LoadAddrPipes_{0} = PipeArray<class _LoadAddr_{0}, ld_req_t<{1}, {2}>, {3}, {4}>;\n"
+    "using LoadValPipes_{0} = PipeArray<class _LoadVal_{0}, {5}, {3}, {4}>;\n"
+    "using StoreAddrPipes_{0} = PipeArray<class _StoreAddr_{0}, st_req_t<{2}>, {3}, {1}, 2>;\n"
+    "using StoreValPipes_{0} = PipeArray<class _StoreVal_{0}, {5}, {3}, {1}>;\n",
+    MemDep.id, MemDep.numStores, MemDep.maxLoopDepth, kReqPipeDepth, MemDep.numLoads, MemDep.cType);
+
+  return res;
+}
+
+
+
+json::Array getMemoryDependenciesJson(DynamicLoopFusionAnalysis &DLFA) {
+  auto Res = json::Array();
+  for (auto &[id, memDepInfo] : DLFA.getMemoryDependencyInfo()) {
+    json::Object infoJson;
+    infoJson["id"] = int(id);
+    infoJson["structDef"] = getDepInfoStructDef(memDepInfo);
+    infoJson["pipeDefs"] = getPipeDefString(memDepInfo);
+    Res.push_back(std::move(infoJson));
+  }
+
+  return Res;
+}
+
+json::Array
+getPipeCallsInAguJson(DecoupledLoopInfo &DecoupleInfo,
+                      MapVector<int, MemoryDependencyInfo> &MemDepInfos) {
+  auto Res = json::Array();
+
+  for (auto &LdReq : DecoupleInfo.loads) {
+    auto MemDep = MemDepInfos[LdReq.memoryId];
+    int ReqId = MemDep.loadReqIds[LdReq.memOp];
+
+    json::Object infoJson;
+    std::string pipeCallStr;
+    llvm::raw_string_ostream O(pipeCallStr);
+    O << llvm::formatv("ld_req_t<{0}, {1}> ld_req_{2}_{3} (0u);\n"
+                       "InitBundle(ld_req_{2}_{3}.sched, 0u);\n"
+                       "InitBundle(ld_req_{2}_{3}.posDepDist, false);\n"
+                       "InitBundle(ld_req_{2}_{3}.isMaxIter, false);\n"
+                       "LoadAddrPipes_{1}::PipeAt<{3}>::write(ld_req_{2}_{3});\n",
+                       MemDep.numStores, MemDep.maxLoopDepth, MemDep.id, ReqId);
+
+    infoJson["instructionIdx"] = getIndexIntoParent(LdReq.memOp);
+    infoJson["instructionBasicBlockIdx"] =
+        getIndexIntoParent(LdReq.memOp->getParent());
+    infoJson["isWrite"] = true;
+    infoJson["pipeCall"] = pipeCallStr;
+    Res.push_back(std::move(infoJson));
+  }
+
+  for (auto &StReq : DecoupleInfo.stores) {
+    auto MemDep = MemDepInfos[StReq.memoryId];
+    int ReqId = MemDep.storeReqIds[StReq.memOp];
+
+    json::Object infoJson;
+    std::string pipeCallStr;
+    llvm::raw_string_ostream O(pipeCallStr);
+    O << llvm::formatv("st_req_t<{0}> st_req_{1}_{2} (0u);\n"
+                       "InitBundle(st_req_{1}_{2}.sched, 0u);\n"
+                       "InitBundle(st_req_{1}_{2}.isMaxIter, false);\n"
+                       "StoreAddrPipes_{1}::PipeAt<{2}, 0>::write(st_req_{1}_{2});\n"
+                       "StoreAddrPipes_{1}::PipeAt<{2}, 1>::write(st_req_{1}_{2});\n",
+                       MemDep.maxLoopDepth, MemDep.id, ReqId);
+
+    infoJson["instructionIdx"] = getIndexIntoParent(StReq.memOp);
+    infoJson["instructionBasicBlockIdx"] =
+        getIndexIntoParent(StReq.memOp->getParent());
+    infoJson["isWrite"] = true;
+    infoJson["pipeCall"] = pipeCallStr;
+    Res.push_back(std::move(infoJson));
+  }
+
+  return Res;
+}
+
+json::Array
+getPipeCallsInComputeJson(DecoupledLoopInfo &DecoupleInfo,
+                      MapVector<int, MemoryDependencyInfo> &MemDepInfos) {
+  auto Res = json::Array();
+
+  for (auto &LdReq : DecoupleInfo.loads) {
+    auto MemDep = MemDepInfos[LdReq.memoryId];
+    int ReqId = MemDep.loadReqIds[LdReq.memOp];
+
+    json::Object infoJson;
+    std::string pipeCallStr;
+    llvm::raw_string_ostream O(pipeCallStr);
+    O << llvm::formatv(
+        "auto _ldVal_{0}_{1} = LoadValPipes_{0}::PipeAt<{1}>::read();\n",
+        MemDep.id, ReqId);
+
+    infoJson["instructionIdx"] = getIndexIntoParent(LdReq.memOp);
+    infoJson["instructionBasicBlockIdx"] =
+        getIndexIntoParent(LdReq.memOp->getParent());
+    infoJson["isWrite"] = false;
+    infoJson["pipeCall"] = pipeCallStr;
+    Res.push_back(std::move(infoJson));
+  }
+
+  for (auto &StReq : DecoupleInfo.stores) {
+    auto MemDep = MemDepInfos[StReq.memoryId];
+    int ReqId = MemDep.storeReqIds[StReq.memOp];
+
+    json::Object infoJson;
+    std::string pipeCallStr;
+    llvm::raw_string_ostream O(pipeCallStr);
+    O << llvm::formatv("StoreValPipes_{0}::PipeAt<{1}>::write();\n", MemDep.id,
+                       ReqId);
+
+    infoJson["instructionIdx"] = getIndexIntoParent(StReq.memOp);
+    infoJson["instructionBasicBlockIdx"] =
+        getIndexIntoParent(StReq.memOp->getParent());
+    infoJson["isWrite"] = true;
+    infoJson["pipeCall"] = pipeCallStr;
+    Res.push_back(std::move(infoJson));
+  }
+
+  return Res;
+}
+
+json::Array getLoopsToDecoupleJson(DynamicLoopFusionAnalysis &DLFA,
+                                   LoopInfo &LI) {
+  auto Res = json::Array();
+
+  auto memDepInfo = DLFA.getMemoryDependencyInfo();
+  for (auto &decoupleInfo : DLFA.getLoopsToDecouple()) {
+    json::Object info;
+    info["type"] = "compute";
+    info["id"] = decoupleInfo.id;
+    info["innerLoopBlockId"] =
+        getIndexIntoParent(decoupleInfo.inner()->getHeader());
+    info["pipeCalls"] = getPipeCallsInComputeJson(decoupleInfo, memDepInfo);
+    Res.push_back(std::move(info));
+  }
+  for (auto &decoupleInfo : DLFA.getAgusToDecouple()) {
+    json::Object info;
+    info["type"] = "agu";
+    info["id"] = decoupleInfo.id;
+    info["innerLoopBlockId"] =
+        getIndexIntoParent(decoupleInfo.inner()->getHeader());
+    info["pipeCalls"] = getPipeCallsInAguJson(decoupleInfo, memDepInfo);
+    Res.push_back(std::move(info));
+  }
+
   return Res;
 }
 
@@ -155,14 +245,14 @@ struct DynamicLoopFusionAnalysisPrinter
       auto &LI = AM.getResult<LoopAnalysis>(F);
       auto &SE = AM.getResult<ScalarEvolutionAnalysis>(F);
       auto *DLFA = new DynamicLoopFusionAnalysis(LI, SE);
+  
+      json::Object report;
+      report["mainKernelName"] = demangle(std::string(F.getName()));
+      report["kernelStartLine"] = F.getSubprogram()->getLine();
+      report["memoryToProtect"] = getMemoryDependenciesJson(*DLFA);
+      report["loopsToDecouple"] = getLoopsToDecoupleJson(*DLFA, LI);
 
-      auto DepInfos = getMemoryDependencyInfo(*DLFA, LI);
-      for (auto Info : DepInfos) {
-        Info.print(errs());
-      }
-
-
-      // outs() << formatv("{0:2}", json::Value(std::move(reportJson))) << "\n";
+      outs() << formatv("{0:2}", json::Value(std::move(report))) << "\n";
     }
 
     return PreservedAnalyses::all();
